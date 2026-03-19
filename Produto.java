78e54f9110cb686a4f030e827d141eab61ce4cc6
Thalyta Monteiro <Thalytamonteir@gmail.com>
2026-03-19 20:36:42 -0300

    aprendendo classe

---
+++ Produto.java
@@ -0,0 +1,32 @@
+public class Produto {
+
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco){
+
+        this.nome= nome;
+        this.preco= preco;
+    }
+
+    public Produto(String nome){
+
+        this.nome= nome;
+        this.preco= 5.0;
+    }
+
+    public void exibirDetalhes(){
+        //sout
+        System.out.println("Nome Produto: " + nome + " \t"+ "Preco: " + preco);
+        
+    }
+     //psvm
+    public static void main(String[] args) {
+        Produto p1= new Produto("caderno", 10.0);
+        Produto p2= new Produto("Thalyta");
+        p1.exibirDetalhes();
+        p2.exibirDetalhes();
+        
+
+    }
+}
